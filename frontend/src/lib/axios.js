4a8507f404bf3eaf5af0ff191025735a4e0426ab
Ten Cua Ban <ban@example.com>
2025-12-08 20:10:40 +0700

    Update Backend URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api" // Khi chạy ở máy tính thì dùng cái này
+    : "https://mochi-planner.onrender.com/api"; // <--- DÁN LINK CỦA BẠN VÀO ĐÂY (Nhớ thêm /api)
 
 const api = axios.create({
   baseURL: BASE_URL,
